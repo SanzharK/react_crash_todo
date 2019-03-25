30c548513ea45cc23b5cd05a29269b559b961374
Sanzhar Kushekbayev <sanzhar.kushekbayev@gmail.com>
2019-03-25 16:28:22 +1100

    added dynamic id creation, creation of new todo item, deletion, completed marking

--- src/App.js
+++ src/App.js
@@ -1,29 +1,63 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header'
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import uuid from 'uuid'
+
 import './App.css';
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
-        title: "take out the trash"
+        id: uuid.v4(),
+        title: "take out the trash",
+        completed: false
       },
       {
-        id: 2,
-        title: "learn react"
+        id: uuid.v4(),
+        title: "learn react",
+        completed: false
       },
       {
-        id: 3,
-        title: "dinner with friends"
+        id: uuid.v4(),
+        title: "dinner with friends",
+        completed: false
       }
     ]
+  }
+
+  // Toggle complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    }) })
+  }
+
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState ({ todos: [...this.state.todos, newTodo] })
   }
 
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        </div>
       </div>
     );
   }
